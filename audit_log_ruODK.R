8347edb41b853c17f2163f872132f14ce09f03a9
chrissyhroberts <chrissyhroberts@yahoo.co.uk>
2023-07-12 14:36:51 +0100

    fixes issue with lists of changes inside a group
    This version fixes an issue where a large number of simultaneous changes were being excluded from the output because they were notarised inside nested lists and lists of lists within the data structure downloaded from Central.
    
    The approach to getting the list of diffs was completely rewritten to account for this.

--- audit_log_ruODK.R
+++ audit_log_ruODK.R
@@ -23,8 +23,8 @@ library(askpass)
 # ruODK setup
 ##############################################################################
 ruODK::ru_setup(
-  url = "ODK_CENTRAL_URL_HERE",
-  un = "ODK_CENTRAL_USERNAME_HERE",
+  url = "YOUR_CENTRAL_URL",
+  un = "YOUR_CENTRAL_USERNAME",
   pw = askpass("please enter your password"),
   tz = "Europe/London",
   verbose = FALSE
@@ -36,6 +36,15 @@ ruODK::ru_setup(
 # START OF FUNCTIONS
 ##############################################################################
 ##############################################################################
+
+##############################################################################
+# Define a function that replaces NULLs in lists, allowing variables to be respected even when null
+##############################################################################
+
+replace_null <- function(x){
+  x <- purrr::map(x, ~ replace(.x, is.null(.x), NA_character_))
+  purrr::map(x, ~ if(is.list(.x)) replace_null(.x) else .x)
+}
 
 ##############################################################################
 # Define an adapted version of ruODK's function which gets the basic audit, but with extra metadata
@@ -99,6 +108,30 @@ get_one_submission_comments<-function (iid, pid = get_default_pid(), fid = get_d
                                                                                      url, un, pw) %>% httr::content(.)
 
 }
+##############################################################################
+#create a function to fully unlist changes because the diffs are nested lists of lists
+# especially when multiple changes are made within a group
+##############################################################################
+
+audit_unlister<-function(x){
+  curr_path = str_c(x$path,collapse = "/")
+  old_list = unlist(x$old)
+  new_list = unlist(x$new)
+  path_depth = names(unlist(x$new))
+
+  out = tibble(old = old_list,
+               new = new_list,
+               path = str_c(curr_path,path_depth,sep = "/")
+  )
+
+  if(is.null(old_list)){out$old=NA}
+  if(is.null(new_list)){out$new=NA}
+  if(is.null(curr_path)){out$path=NA}
+
+  out<-select(out,old,new,path)
+
+  return(out)
+}
 
 ##############################################################################
 # Define function which creates a report that amalgamates data from the three sources
@@ -113,11 +146,11 @@ get_audit_report_one_submission<-function(iid,
                                           retries = get_retries()){
 
   # get the audit for the submission
-  a<-get_one_submission_audit_detailed(iid,pid,fid,url,un,pw,retries)
+  a<-replace_null(get_one_submission_audit_detailed(iid,pid,fid,url,un,pw,retries))
   # get the diffs for a single submission
-  b<-get_one_submission_diffs(iid,pid,fid,url,un,pw,retries)
+  b<-replace_null(get_one_submission_diffs(iid,pid,fid,url,un,pw,retries))
   # get the comments from the edits
-  d<-get_one_submission_comments(iid,pid,fid,url,un,pw,retries)
+  d<-replace_null(get_one_submission_comments(iid,pid,fid,url,un,pw,retries))
 
   # convert the audit to a tibble providing a 'change number' to allow tidy pivot because ODK Central doesn't provide a truly unique hex
   x<-tibble(action = names(unlist(a)),
@@ -134,20 +167,57 @@ get_audit_report_one_submission<-function(iid,
   # if there have been edits, then turn the list of changes and the list of edit comments in to a tibble,
   # using duplicate row detection to find the start of each group of linked edits (i.e. to find all rows related to a single editing session)
 
-    if(length(b)>0){
-    y<-tibble(uuid = names(unlist(b)),
-              value = unlist(b)) %>%
-      separate_wider_delim(uuid,delim = ".",names = c("uuid","stat")) %>%
-      group_by(stat) %>%
-      mutate(duplicateID = row_number(),
-             field.change.no = case_when(stat == "old" ~ duplicateID),
-      ) %>%
-      ungroup() %>%
-      fill(field.change.no) %>%
-      select(-duplicateID) %>%
-      distinct() %>%
-      pivot_wider(id_cols = c(uuid,field.change.no),names_from = stat,values_from = value,values_fn = list)
+  if(length(b)>0){
+    y<- tibble(uuid = character(),old=character(),new=character(),path=character())
 
+    # Loop through all levels of the file and unnest the list of lists
+    for(i in 1:length(b)){
+      #    print(i)
+      mother = b[[i]]
+      #    print(mother)
+      for(j in 1:length(mother)){
+        #      print(j)
+        tmp = audit_unlister(mother[[j]])
+        tmp$uuid <- names(b)[i]
+        y = bind_rows(y,tmp)
+      }
+    }
+  }
+
+
+  # If there have been no edits, then make dummy variables for the initial submission,
+  # so that the table can be merged with ones where there are edits
+  if(length(b)==0){
+
+    y<- x %>% mutate (
+      acteeId = NA,
+      old = NA,
+      new = NA,
+      path = NA,
+      comment = NA,
+      actor.updatedAt = NA
+    )
+  }
+
+
+
+  # build the final df, for cases where there are edits
+  if(length(b)>0){
+    fin<-   full_join(x,y) %>%
+      arrange(loggedAt)
+
+    if(length(d)==0){fin<-fin %>% mutate(comment=NA)}
+  }
+
+  # build the final df, for cases where there are no edits
+  if(length(b)==0){fin<-y}
+
+
+
+
+
+  if(length(d)>0){
+    # create the comment rows
     yy<-tibble(stat = names(unlist(d)),
                value = unlist(d))%>%
       group_by(stat) %>%
@@ -167,14 +237,13 @@ get_audit_report_one_submission<-function(iid,
         old = NA,
         new = NA,
         path = NA,
-        path1 = NA,
-        path2 = NA,
         actor.displayName = as.character(actor.displayName),
         actor.createdAt=as.character(actor.createdAt),
         actor.type = as.character(actor.type),
         actorId = as.character(actorId),
         actor.id = actorId,
         actor.updatedAt = as.character(actor.updatedAt),
+        actor.deletedAt = as.character(actor.deletedAt),
         acteeID = NA,
         details.submissionId = NA,
         comment = body
@@ -183,60 +252,38 @@ get_audit_report_one_submission<-function(iid,
         -duplicateID,
         -body
       )
-  }
 
-  # If there have been no edits, then make dummy variables for the initial submission,
-  # so that the table can be merged with ones where there are edits
-  if(length(b)==0){
-
-    y<- x %>%
-      mutate (field.change.no=1,
-              acteeID=NA,
-              old=NA,
-              new=NA,
-              path=NA,
-              path1=NA,
-              path2=NA,
-              comment=NA,
-              actor.updatedAt=NA,
-              old=NA,
-              new=NA,
-              path=NA,
-              path1=NA,
-              path2=NA
-      )
-  }
-
-
-# build the final df, for cases where there are edits
-  if(length(b)>0){
-    fin<-full_join(x,y) %>% arrange(loggedAt,actor.no.internal)
+    #bind the comment rows
     fin<-bind_rows(fin,yy)
   }
-# build the final df, for cases where there are no edits
-    if(length(b)==0){fin<-y}
 
-# arrange the df by date of the event, add in the project and form IDs
-# sort the columns in a sensible way
-  fin <- fin %>%
-    arrange(loggedAt) %>%
+  # sort columns
+
+  fin<- fin %>%
     mutate(
-      fid=fid,
-      pid=pid
+      project_id = pid,
+      form_id = fid
     ) %>%
-    select(pid,
-           fid,
-           details.submissionId,
-           loggedAt,
-           uuid,
-           comment,
-           action,
-           old:path2,
-           actor.displayName,
-           actor.type,
-           actor.id,
-           everything()
-    )
+    select(
+      project_id,
+      form_id,
+      details.submissionId,
+      loggedAt,
+      uuid,
+      comment,
+      action,
+      path,
+      old,
+      new,
+      actor.displayName,
+      actor.type,
+      actor.id,
+      everything()
+    ) %>%
+    #    mutate_all(as.character) %>%
+    arrange(loggedAt)
+
+
 
   return(fin)
 }
@@ -254,11 +301,18 @@ get_audit_report_one_submission<-function(iid,
 
 # First Get a list of all submissions for a specified form
 
-sub.list<-submission_list(pid = 37,fid = "geopoint_map")
+sub.list<-submission_list(pid = 37,fid = "period.diary")
 sub.list<-sub.list$instance_id
 
 #Build an initial df, using the first submission ID from sub.list
-df<-get_audit_report_one_submission(iid = sub.list[1],pid = 37,fid = "geopoint_map")
+df<-get_audit_report_one_submission(iid = sub.list[1],pid = 37,fid = "period.diary")
 #Bind rows of all subsequent submission IDs from the sub.list
-for(i in 2:length(sub.list)){try(df <- bind_rows(df,get_audit_report_one_submission(iid = sub.list[i],pid = 37,fid = "geopoint_map")))}
+for(i in 2:length(sub.list)){
+  message(str_c("getting audit ",i," of ", length(sub.list)))
+  try(df <- bind_rows(df,get_audit_report_one_submission(iid = sub.list[i],pid = 37,fid = "period.diary")))}
 
+#Arrange by date
+df<-df %>% arrange(loggedAt)
+
+
+write_csv(df,"example_audit.csv")
